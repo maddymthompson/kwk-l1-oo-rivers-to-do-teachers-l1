2add9d74d494d5d1a0306dd710ccc9a0861fbef3
maddymthompson <madeline.thompson.17@tfacorps.org>
2018-07-13 21:25:02 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -1,26 +1,26 @@
-Rivers have a name (that should be set at initialization). A river also has a
-length, countries that it flows through (:countries), and discharge (in cubic meters per
-second.) Set these attributes using writer methods.
-
-A river also has a few actions. `flood` should increase the discharge by 30%.
-`dry_up` should decrease the discharge by 50%.
-
-Create the `River` class based on this data and then create instances of the class for the following rivers (using your awesome Wikipedia skills):
-
-+ The Nile
-
-+ The Mississippi
-
-+ The Amazon
-
-+ The Seine
-
-+ The Yangtze
-
-+ The Euphrates
-
-The Amazon and the Yangtze should flood. The Euphrates should dry up.
-(name, student population, mascot, type)
+# Rivers have a name (that should be set at initialization). A river also has a
+# length, countries that it flows through (:countries), and discharge (in cubic meters per
+# second.) Set these attributes using writer methods.
+#
+# A river also has a few actions. `flood` should increase the discharge by 30%.
+# `dry_up` should decrease the discharge by 50%.
+#
+# Create the `River` class based on this data and then create instances of the class for the following rivers (using your awesome Wikipedia skills):
+#
+# + The Nile
+#
+# + The Mississippi
+#
+# + The Amazon
+#
+# + The Seine
+#
+# + The Yangtze
+#
+# + The Euphrates
+#
+# The Amazon and the Yangtze should flood. The Euphrates should dry up.
+# (name, student population, mascot, type)
 
 
 
